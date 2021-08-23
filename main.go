fe6e9e8773dc6622c708bdc80ea8f5ea89d33001
Moses Ichado <ichadomoses@gmail.com>
2021-08-23 15:39:45 -0700

    math function

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main (){
 	fmt.Println("Hello world")
+	work()
+}
+func work(){
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(144))
 }
