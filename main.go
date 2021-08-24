f42d9b31e98c07e24ca220fa98f4964c04ab5bc5
Moses Ichado <ichadomoses@gmail.com>
2021-08-23 17:07:59 -0700

    fruit commit

--- main.go
+++ main.go
@@ -4,11 +4,28 @@ import (
 	"math"
 )
 
-func main (){
+
+func main() {
 	fmt.Println("Hello world")
 	work()
+	anything(  "Moses" , 34)
+
+	t, h  := fruit("orange", 500)
+	fmt.Println(t,h)
 }
-func work(){
+func anything (name string, age int)( string, int){
+	fmt.Println("name is", name)
+	fmt.Println("age is", age)
+
+	return name, age
+}
+func fruit (name string, number int)(string, int){
+   number = number + 3
+	return name, number
+}
+
+
+func work() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(144))
